cf428be2850b00cd0c96eb8ae906a9db10855774
KimHyoJeong <31529142+zunghyo@users.noreply.github.com>
2024-01-02 10:17:20 +0900

    Time: 0 ms (100.00%), Space: 45.3 MB (34.75%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start <= end) {
+            int middle = start + (end - start) / 2;
+            
+            if(nums[middle] == target) {
+                return middle;
+            } else if(nums[middle] > target) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
