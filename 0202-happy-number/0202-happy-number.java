0305c04114c0500b4f4e4207912a2b6a9d00e20b
KimHyoJeong <31529142+zunghyo@users.noreply.github.com>
2024-01-30 08:36:27 +0900

    Time: 1 ms (75.98%), Space: 41.3 MB (8.42%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> visited = new HashSet<>();
+        
+        while(n > 1 && !visited.contains(n)) {
+            visited.add(n);
+            
+            int sum = 0;
+            while(n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n / 10;
+            }
+            n = sum;
+        }
+        
+        return n == 1;
+    }
+}
